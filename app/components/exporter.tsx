83161406fbf37bbd682fdd47fa2ed472d9099ef0
Meng <vow.meng@mspbots.ai>
2023-08-16 12:16:26 +0800

    更新最新数据

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -475,9 +475,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>Openai Vow</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/VowSir/openai-vow
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
